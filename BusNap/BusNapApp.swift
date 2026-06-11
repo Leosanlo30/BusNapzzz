2a12e78d244f609f6227cf9e24bc9cd4234c5b7e
leosanlo30 <a25216404@alumnos.uady.edu.mx>
2026-06-10 21:08:33 -0600

    Initial Commit

---
+++ BusNap/BusNapApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusNapApp.swift
+//  BusNap
+//
+//  Created by Leonardo Ariel San Martin Lopez  on 10/06/26.
+//
+
+import SwiftUI
+
+@main
+struct BusNapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BusNapTests/BusNapTests.swift
@@ -0,0 +1,17 @@
+//
+//  BusNapTests.swift
+//  BusNapTests
+//
+//  Created by Leonardo Ariel San Martin Lopez  on 10/06/26.
+//
+
+import Testing
+@testable import BusNap
+
+struct BusNapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
