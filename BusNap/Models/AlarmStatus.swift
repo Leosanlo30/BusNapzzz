518b1cd074a98fe5506a4ffb9f50f41a10a3eed8
leosanlo30 <a25216404@alumnos.uady.edu.mx>
2026-06-25 13:45:23 -0600

    AlarmStatus and AlarmStatusTests: Data Type AlarmStatus and its Tests

--- BusNap/Models/AlarmStatus.swift
+++ BusNap/Models/AlarmStatus.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+//Tipo de dato del Estado de la alarma
+enum AlarmStatus: Equatable {
+    case inactive
+    case configured
+    case monitoring
+    case criticalZone
+    case triggered
+    case cancelled
+    
+    //Devuelve true si el viaje esta en progreso y false si no lo
+    var isActiveTrip: Bool {
+        switch self {
+        case .monitoring, .criticalZone:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    //Mensaje para la UI
+    var displayStatus: String{
+        
+    switch self {
+    case .inactive:
+        return "Inactiva"
+    case .configured:
+        return "Viaje configurado"
+    case .monitoring:
+        return "Monitoreando ruta..."
+    case .criticalZone:
+        return "Llegando al destino"
+    case .triggered:
+        return "Despierta"
+    case .cancelled:
+        return "Viaje cancelado"
+        }
+       
+    }
+    
+    
+    
+    }
+
+
--- BusNapTests/AlarmStatusTests.swift
+++ BusNapTests/AlarmStatusTests.swift
@@ -5,4 +5,39 @@
 //  Created by Leonardo Ariel San Martin Lopez  on 23/06/26.
 //
 
-import Foundation
+import XCTest
+@testable import BusNap
+
+final class AlarmStatusTests: XCTestCase {
+
+    func testIsActiveTripReturnsTrueForActiveStates() {
+        // Arrange
+        let monitoringState = AlarmStatus.monitoring
+        let criticalState = AlarmStatus.criticalZone
+        
+        // Assert: Comprobamos que ambos estados se consideren "Activos" monitoring y criticalZone
+        XCTAssertTrue(monitoringState.isActiveTrip, "El estado monitoring debe ser un viaje activo")
+        XCTAssertTrue(criticalState.isActiveTrip, "El estado criticalZone debe ser un viaje activo")
+    }
+    
+    func testIsActiveTripReturnsFalseForInactiveStates() {
+        // Arrange
+        //Declaramos varibales para probar que no esten activos las demas propiedades
+        let inactiveState = AlarmStatus.inactive
+        let configuredState = AlarmStatus.configured
+        let triggeredState = AlarmStatus.triggered
+        let cancelledState = AlarmStatus.cancelled
+        
+        // Assert: Comprobamos que el resto de estados NO se consideren activos
+        XCTAssertFalse(inactiveState.isActiveTrip)
+        XCTAssertFalse(configuredState.isActiveTrip)
+        XCTAssertFalse(triggeredState.isActiveTrip)
+        XCTAssertFalse(cancelledState.isActiveTrip)
+    }
+    
+    func testDisplayStatusReturnsCorrectText() {
+        // Arrange & Assert: Validamos el texto de la funcion coincida con la salida esperada para la UI
+        XCTAssertEqual(AlarmStatus.monitoring.displayStatus, "Monitoreando ruta...")
+        XCTAssertEqual(AlarmStatus.triggered.displayStatus, "Despierta")
+    }
+}
